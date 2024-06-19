957e7e6694eb90ad6e06c339aea6b1a9cd8b2f64
Icily57 <icilyweird57@gmail.com>
2024-06-19 13:21:04 +0300

    Profile card

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,28 @@ import ProfileCard from './Components/ProfileCard'
 
 function App() {
   const name =`Mikaela Kamunya`
-  const profilePicture = './images/mikaela.jpg'
+  const profilePicture = './assets/mikaela.jpg'
   const moreInfo = {
     age: '23 years old',
     bio:'I am a software engineering student at Teach2give. I am passionate about technology '
   }
 
   const name2 =`Truphena Abere`
-  const profilePicture2 = './images/truphena.jpg'
+  const profilePicture2 = './assets/truphena.jpg'
   const moreInfo2 = {
     age: '21 years old',
     bio:'I am a computer science student at Dedan Kimathi University . I am love programming '
   }
 
   const name3 =`Mark Gatere`
-  const profilePicture3 = './images/gatere.jpg'
+  const profilePicture3 = './assets/gatere.jpg'
   const moreInfo3 = {
     age: '22 years old',
     bio:'I am a meta dev at Microsoft. I am passionate about AI and Machine Learning '
   }
 
   const name4 =`Harun Mbaabu`
-  const profilePicture4 = './images/harun.jpg'
+  const profilePicture4 = './assets/harun.jpg'
   const moreInfo4 = {
     age: '30 years old',
     bio:'I am a data engineer. I am Building my own company '
@@ -33,12 +33,12 @@ function App() {
   }
 
   return (
-    <>
-           <ProfileCard name={name} profilePicture={profilePicture} moreInfo={moreInfo}/>
+    <div className='container'>
+           <ProfileCard name={name} profilePicture={profilePicture} moreInfo={moreInfo}/> 
             <ProfileCard name={name2} profilePicture={profilePicture2} moreInfo={moreInfo2}/>
             <ProfileCard name={name3} profilePicture={profilePicture3} moreInfo={moreInfo3}/>
             <ProfileCard name={name4} profilePicture={profilePicture4} moreInfo={moreInfo4}/>
-    </>
+    </div>
   )
 }
 
--- src/Components/ProfileCard.jsx
+++ src/Components/ProfileCard.jsx
@@ -1,13 +1,13 @@
-
+import './ProfileCard.css';
 
 const ProfileCard=({name,profilePicture,moreInfo})=>{
     return(
-        <>
-        <h2>Hello my name is  {name}</h2>
-        <img src = {profilePicture} alt = {name}/>
-        <p>I am {moreInfo.age}</p>
-        <p>{moreInfo.bio}</p>
-        </>
+        <div className='ProfileCard'>
+        <h2 className='h2'>Hello my name is  {name}</h2>
+        <img className='pic' src = {profilePicture} alt = {name}/>
+        <p className='age'>I am {moreInfo.age}</p>
+        <p className='bio'>{moreInfo.bio}</p>
+        </div>
     )
 }
 
